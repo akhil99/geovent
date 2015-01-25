b16e840054b47716fc79a30d538c5f18b4706cb8
Akhil Palla <code@theakhil.com>
2015-01-24 21:22:10 -0800

    Added map support

--- android/app/src/main/java/com/buildncode/geovent/MapActivity.java
+++ android/app/src/main/java/com/buildncode/geovent/MapActivity.java
@@ -1,25 +1,93 @@
 package com.buildncode.geovent;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
 import com.mapbox.mapboxsdk.views.MapView;
 
+import java.text.DateFormat;
 
-public class MapActivity extends ActionBarActivity{
+
+public class MapActivity extends ActionBarActivity implements OnMapReadyCallback, LocationListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     MapView mapView;
+    GoogleMap map;
+
+    GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
-        initMapView();
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        buildGoogleApiClient();
     }
 
-    private void initMapView(){
-        mapView = (MapView)findViewById(R.id.mapview);
-        mapView.setUserLocationEnabled(true);
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        mGoogleApiClient.connect();
     }
 
+    protected void startLocationUpdates() {
+        LocationRequest r = LocationRequest.create();
+        r.setInterval(10000);
+        r.setFastestInterval(5000);
+        r.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, r, this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        googleMap.setMyLocationEnabled(true);
+        map = googleMap;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.d("haxor", "location changed");
+        LatLng latlng = new LatLng(location.getLatitude(), location.getLongitude());
+        if(map != null)map.moveCamera(CameraUpdateFactory.newLatLng(latlng));
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.d("haxor", "connected");
+        map.moveCamera(CameraUpdateFactory.zoomTo(17));
+        startLocationUpdates();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    protected void onDestroy(){
+        super.onDestroy();
+        if(mGoogleApiClient != null)mGoogleApiClient.disconnect();
+    }
 }
