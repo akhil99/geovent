a4c31cce45b2b761eb443aa55d128c6b91d1a89b
Akhil Palla <code@theakhil.com>
2015-01-25 00:03:16 -0800

    Added Firebase logging of user position

--- android/app/src/main/java/com/buildncode/geovent/MapActivity.java
+++ android/app/src/main/java/com/buildncode/geovent/MapActivity.java
@@ -1,37 +1,43 @@
 package com.buildncode.geovent;
 
 import android.location.Location;
+import android.media.CameraProfile;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 
+import com.firebase.client.Firebase;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.mapbox.mapboxsdk.views.MapView;
+import com.parse.ParseUser;
 
-import java.text.DateFormat;
+import org.json.JSONObject;
 
 
 public class MapActivity extends ActionBarActivity implements OnMapReadyCallback, LocationListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
-    MapView mapView;
     GoogleMap map;
-
+    Firebase myFirebaseRef;
     GoogleApiClient mGoogleApiClient;
+    ParseUser user;
+
+    boolean mapMoved = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        myFirebaseRef = new Firebase("https://geovent.firebaseio.com/");
+        user = ParseUser.getCurrentUser();
         MapFragment mapFragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -50,7 +56,7 @@ public class MapActivity extends ActionBarActivity implements OnMapReadyCallback
     protected void startLocationUpdates() {
         LocationRequest r = LocationRequest.create();
         r.setInterval(10000);
-        r.setFastestInterval(5000);
+        r.setFastestInterval(1000);
         r.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
         LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, r, this);
     }
@@ -66,7 +72,13 @@ public class MapActivity extends ActionBarActivity implements OnMapReadyCallback
     public void onLocationChanged(Location location) {
         Log.d("haxor", "location changed");
         LatLng latlng = new LatLng(location.getLatitude(), location.getLongitude());
-        if(map != null)map.moveCamera(CameraUpdateFactory.newLatLng(latlng));
+        JSONObject data = new JSONObject();
+        myFirebaseRef.child("users").child(user.getObjectId()).child("latitude").setValue(location.getLatitude());
+        myFirebaseRef.child("users").child(user.getObjectId()).child("longitude").setValue(location.getLongitude());
+        if(map != null && !mapMoved){
+            map.moveCamera(CameraUpdateFactory.newLatLng(latlng));
+            mapMoved = true;
+        }
     }
 
     @Override
